91f2c1b019387f1a7e38f2404c6da55af441612a
Alperen <alperensenerr>
2022-06-20 22:30:51 +0300

    confision matrix update

--- Logistic regression.py
+++ Logistic regression.py
@@ -1,22 +1,21 @@
 import numpy as np
 import matplotlib as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 veriler =pd.read_csv('veriler.csv')
 
 data = veriler.iloc[:,1:4].values
 cinsiyet = veriler.iloc[:,4:].values
 
-from sklearn.model_selection import train_test_split
-
 data_train, data_test, cinsiyet_train, cinsiyet_test = train_test_split(data,cinsiyet,test_size=0.33,random_state=0)
-
-from sklearn.linear_model import LogisticRegression
 
 logr = LogisticRegression(random_state=0)
 logr.fit(data_train,cinsiyet_train)
 
 y_pred = logr.predict(data_test)
 
-print(y_pred)
-print(cinsiyet_test)
+cm = confusion_matrix(cinsiyet_test,y_pred)
+print(cm)
